08e74acfba67585e829e9b03034b886b95743d97
TamiaNCAT <78436643+TamiaNCAT@users.noreply.github.com>
2021-04-26 02:21:30 -0400

    Added the php files to a separate folder

---
+++ COVID BUDDY/php/checkout_add.php
@@ -0,0 +1,36 @@
+<?php
+
+$servername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "COVID_BUDDY";
+
+
+// Create Connection
+$conn = new mysqli($servername, $dbUsername, $dbPassword, $dbName);
+
+if(!$conn){
+    die("Connection failed: ".mysqli_connect_error());
+}
+
+
+
+        $checkEmail = $_POST['checkEmail'];
+        $checkPassword = $_POST['checkPassword'];
+        $checkAddress = $_POST['checkAddress'];
+        $checkAddress2 = $_POST['checkAddress2'];
+        $checkCity = $_POST['checkCity'];
+        $checkZip = $_POST['checkZip'];
+        $checkCCH = $_POST['checkCCH'];
+        $checkCCN = $_POST['checkCCN'];
+
+
+        $sql = "INSERT INTO `Checkout` (`checkID`, `checkEmail`, `checkPassword`, `checkAddress`, `checkAddress2`, `checkCity`, `checkZip`, `checkCCH`, `checkCCN`) VALUES (NULL, '$checkEmail', '$checkPassword', '$checkAddress', '$checkAddress2', '$checkCity', '$checkZip', '$checkCCH', '$checkCCN')";
+
+if ($conn->query($sql) === TRUE){
+    header("Location: ../pages/checkout_complete.html?Success");
+    echo"<script>alert('Doggo Adopted!');</script>";
+} else {
+    echo"ERROR: " . $sql . "<br>" . $conn->error;
+}
+?>
